df0f16d99dfe3047cba8d3c77e9bdb8ec44ec367
Bryan de Bourbon <bryandebourbon@gmail.com>
2025-01-01 20:57:55 -0500

    Initial Commit

---
+++ ota-ios/ota_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ota_iosApp.swift
+//  ota-ios
+//
+//  Created by Bryan de Bourbon on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ota_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
